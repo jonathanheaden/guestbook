4d35541a1008d1fb7202a54a11faa73d0b134e51
Jonathan Headen <jonathan.headen@outlook.com>
2016-01-14 07:29:45 +1100

    added guest comment ability

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -3,16 +3,32 @@
             [guestbook.views.layout :as layout]
             [hiccup.form :refer :all]))
 
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]]
+     )])
 
-(defn home []
+(defn home [& [name message error]]
   (layout/common
    [:h1 "Guestbook"]
-   [:p "Welcome to my guestbook"] [:hr]
-   [:form
-    [:p "Name:"]
-    [:input]
-    [:p "Message:"]
-    [:textarea {:rows 10 :cols 40}]]))
+   [:p "Welcome to my guestbook"]
+   [:p error]
+   ;here we call our show-guests function
+   ;to generate the list of existing comments (show-guests)
+   [:hr]
+   ;here we create a form with text fields called "name" and "message" ;these will be sent when the form posts to the server as keywords of ;the same name
+   (form-to [:post "/"]
+            [:p "Name:"]
+            (text-field "name" name)
+            [:p "Message:"]
+            (text-area {:rows 10 :cols 40} "message" message) [:br]
+            (submit-button "comment"))))
 
 
 (defroutes home-routes
