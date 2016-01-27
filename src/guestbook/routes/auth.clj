948c9274c81255d17c6f1974b7c93ba83ee989f8
Jonathan Headen <jonathan.headen@outlook.com>
2016-01-27 19:14:51 +1100

    added registration routes function

--- src/guestbook/routes/auth.clj
+++ src/guestbook/routes/auth.clj
@@ -2,7 +2,8 @@
 (:require [compojure.core :refer [defroutes GET POST]]
             [guestbook.views.layout :as layout]
             [hiccup.form :refer
-             [form-to label text-field password-field submit-button]]))
+             [form-to label text-field password-field submit-button]]
+            [noir.response :refer [redirect]]))
 
 (defn control [field name text]
   (list (label name text)
@@ -16,3 +17,11 @@
             (control password-field :pass "Password")
             (control password-field :pass1 "Retype Password")
             (submit-button "Create Account"))))
+
+(defroutes auth-routes
+  (GET "/register" []
+       (registration-page))
+  (POST "/register" [id pass pass1]
+        (if (= pass pass1)
+          (redirect "/")
+          (registration-page))))
